ec4a0c759c0073e0ad8178d9d02ad78e0f8ef6a5
Kalyan <pkalyanjs@gmail.com>
2024-03-13 15:43:37 +0530

    loop code has been added

--- Loops/src/DoWhileInfiniteLoop.java
+++ Loops/src/DoWhileInfiniteLoop.java
@@ -2,7 +2,14 @@
 public class DoWhileInfiniteLoop {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		int x = 10;
+		
+		do {
+			System.out.println("value of x : "+ x);
+			x++;
+			System.out.println("\n");
+		}while(true);
 
 	}
 
